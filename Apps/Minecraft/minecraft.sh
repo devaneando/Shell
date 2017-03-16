6b6ca83ac8c3a4f97aa5b5886320adc7fd905645
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2017-03-16 18:53:07 +0000

    2017-03-16 18:52

--- Apps/Minecraft/minecraft.sh
+++ Apps/Minecraft/minecraft.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env  bash
 cd /data/App/Minecraft
-java -Xmx1G -Xms512M -cp minecraft.jar net.minecraft.bootstrap.Bootstrap
+java -Xmx1G -Xms512M -cp /data/App/Minecraft/Minecraft.jar net.minecraft.bootstrap.Bootstrap
