ed5f4d9649ce3cdb7ac4b2fda3060099b0c7a993
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2017-03-16 10:21:19 +0000

    2017-03-16 10:15

--- Apps/Bash/Aliases/060.ez.bash
+++ Apps/Bash/Aliases/060.ez.bash
@@ -1,12 +1,23 @@
-##### find files in the current directory
-##### $1: search pattern
 function eZClear()
 {
     if [ -d app/cache ]; then
         rm -Rf app/cache/*
     fi
-    /usr/bin/env php app/console cache:clear --env=prod
+
+    if [ -d app ]; then
+        /usr/bin/env php app/console cache:clear --env=prod
+    fi
+
 }
 
+function eZAssets()
+{
 
+    if [ -d app ]; then
+        /usr/bin/env php app/console cache:clear --env=prod
+        /usr/bin/env app/console assetic:dump
+        /usr/bin/env app/console assets:install
+    fi
+
+}
 
