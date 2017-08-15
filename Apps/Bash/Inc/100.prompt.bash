5111938fb5d70b992157ab3b79a1cbc97626ba8f
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2017-08-15 09:23:16 +0100

    2017-08-15 09:23

--- Apps/Bash/Inc/100.prompt.bash
+++ Apps/Bash/Inc/100.prompt.bash
@@ -1,62 +1,412 @@
 #!/usr/bin/env bash
-# vim:set syntax=sh:
+# vim: ft=bash ts=2 sw=2 sts=2
+#
+# agnoster's Theme - https://gist.github.com/3712874
+# A Powerline-inspired theme for BASH
+#
+# (Converted from ZSH theme by Kenny Root)
+# https://gist.github.com/kruton/8345450
+#
+# Updated & fixed by Erik Selberg erik@selberg.org 1/14/17
+# Tested on MacOSX, Ubuntu, Amazon Linux
+# Bash v3 and v4
+#
+# # README
+#
+# In order for this theme to render correctly, you will need a
+# [Powerline-patched font](https://gist.github.com/1595572).
+# I recommend: https://github.com/powerline/fonts.git
+# > git clone https://github.com/powerline/fonts.git fonts
+# > cd fonts
+# > install.sh
 
-__prompt_command()
-{
-	local exitStatus=$?
+# In addition, I recommend the
+# [Solarized theme](https://github.com/altercation/solarized/) and, if you're
+# using it on Mac OS X, [iTerm 2](http://www.iterm2.com/) over Terminal.app -
+# it has significantly better color fidelity.
 
-	PS1_ITALIC='\[\e[3m\]'	
-	PS1_NO_ITALIC='\[\e[23m\]'
-	PS1_RESET='\[\e[0m\]'
-	
+# Install:
 
-	if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-		PS1_SYMBOL='\[\e[92m\]'
-		PS1_TIME='\[\e[32m\]'
-		PS1_HOST='\[\e[32m\]'
-		PS1_GIT='\[\e[92m\]'
-		PS1_FOLDER='\[\e[92m\]'
-		PS1_OK='\[\e[32m\]'
-		PS1_NOK='\[\e[31m\]'		
-	else
-		PS1_SYMBOL='\[\e[96m\]'
-		PS1_TIME='\[\e[34m\]'
-		PS1_HOST='\[\e[34m\]'
-		PS1_GIT='\[\e[96m\]'
-		PS1_FOLDER='\[\e[94m\]'
-		PS1_OK='\[\e[32m\]'
-		PS1_NOK='\[\e[31m\]'
-	fi
+# I recommend the following:
+# $ cd home
+# $ mkdir -p .bash/themes/agnoster-bash
+# $ git clone https://github.com/speedenator/agnoster-bash.git .bash/themes/agnoster-bash
 
-	# The time
-	PS1="${PS1_ITALIC}${PS1_TIME}\A ${PS1_NO_ITALIC}"
+# then add the following to your .bashrc:
 
-	# The git repository
-	gitRepository="$(/usr/bin/git rev-parse --abbrev-ref HEAD 2>/dev/null)";
-    if [ -n "$gitRepository" ]; then
-        PS1+="${PS1_ITALIC}${PS1_GIT}[${gitRepository}] ${PS1_NO_ITALIC}"
+# export THEME=$HOME/.bash/themes/agnoster-bash/agnoster.bash
+# if [[ -f $THEME ]]; then
+#     export DEFAULT_USER=`whoami`
+#     source $THEME
+# fi
+
+#
+# # Goals
+#
+# The aim of this theme is to only show you *relevant* information. Like most
+# prompts, it will only show git information when in a git working directory.
+# However, it goes a step further: everything from the current user and
+# hostname to whether the last call exited with an error to whether background
+# jobs are running in this shell will all be displayed automatically when
+# appropriate.
+
+# Generally speaking, this script has limited support for right
+# prompts (ala powerlevel9k on zsh), but it's pretty problematic in Bash.
+# The general pattern is to write out the right prompt, hit \r, then
+# write the left. This is problematic for the following reasons:
+# - Doesn't properly resize dynamically when you resize the terminal
+# - Changes to the prompt (like clearing and re-typing, super common) deletes the prompt
+# - Getting the right alignment via columns / tput cols is pretty problematic (and is a bug in this version)
+# - Bash prompt escapes (like \h or \w) don't get interpolated
+#
+# all in all, if you really, really want right-side prompts without a
+# ton of work, recommend going to zsh for now. If you know how to fix this,
+# would appreciate it!
+
+# note: requires bash v4+... Mac users - you often have bash3.
+# 'brew install bash' will set you free
+PROMPT_DIRTRIM=2 # bash4 and above
+
+######################################################################
+DEBUG=0
+debug() {
+    if [[ ${DEBUG} -ne 0 ]]; then
+	>&2 echo -e $*
     fi
-	
-	# The hostname of the machine
-	PS1+="${PS1_ITALIC}${PS1_HOST}\h${PS1_NO_ITALIC}"
-	
-	# The divider
-	PS1+="${PS1_ITALIC}${PS1_SYMBOL}@${PS1_NO_ITALIC}"
-	
-    # The folder
-	PS1+="${PS1_ITALIC}${PS1_FOLDER}\W ${PS1_NO_ITALIC}"
-	
-	if [ $exitStatus -eq 0 ]; then
-		PS1+="${PS1_SYMBOL} ${PS1_OK}:)${PS1_RESET} "
-	else
-		PS1+="${PS1_SYMBOL} ${PS1_NOK}:(${PS1_RESET} "
-	fi
-
-    # Appends history data
-    history -a
-    history -c
-    history -r
-	
 }
 
-export PROMPT_COMMAND=__prompt_command
+######################################################################
+### Segment drawing
+# A few utility functions to make it easy and re-usable to draw segmented prompts
+
+CURRENT_BG='NONE'
+CURRENT_RBG='NONE'
+SEGMENT_SEPARATOR=''
+RIGHT_SEPARATOR=''
+LEFT_SUBSEG=''
+RIGHT_SUBSEG=''
+
+text_effect() {
+    case "$1" in
+	reset)     echo 0;;
+	bold)      echo 1;;
+	underline) echo 4;;
+    esac
+}
+
+# to add colors, see
+# http://bitmote.com/index.php?post/2012/11/19/Using-ANSI-Color-Codes-to-Colorize-Your-Bash-Prompt-on-Linux
+# under the "256 (8-bit) Colors" section, and follow the example for orange below
+# See also http://misc.flogisoft.com/bash/tip_colors_and_formatting
+fg_color()
+{
+    case "$1" in
+        default)        echo 39;;
+        black)          echo 30;;
+        red)            echo 31;;
+        red_light)      echo 91;;
+        green)          echo 32;;
+        green_light)    echo 92;;
+        yellow)         echo 33;;
+        yellow_light)   echo 93;;
+        blue)           echo 34;;
+        blue_light)     echo 94;;
+        magenta)        echo 35;;
+        magenta_light)  echo 95;;
+        cyan)           echo 36;;
+        cyan_light)     echo 96;;
+        white)          echo 37;;
+        orange)         echo 38\;5\;166;;
+        gray)           echo 90;;
+        gray_light)     echo 37;;
+    esac
+}
+
+bg_color()
+{
+    case "$1" in
+        default)        echo 49;;
+        black)          echo 40;;
+        red)            echo 41;;
+        red_light)      echo 101;;
+        green)          echo 42;;
+        green_light)    echo 102;;
+        yellow)         echo 43;;
+        yellow_light)   echo 103;;
+        blue)           echo 44;;
+        blue_light)     echo 104;;
+        magenta)        echo 45;;
+        magenta_light)  echo 105;;
+        cyan)           echo 46;;
+        cyan_light)     echo 106;;
+        white)          echo 47;;
+        orange)         echo 48\;5\;166;;
+        gray)           echo 100;;
+        gray_light)     echo 47;;
+    esac;
+}
+
+# TIL: declare is global not local, so best use a different name
+# for codes (mycodes) as otherwise it'll clobber the original.
+# this changes from BASH v3 to BASH v4.
+ansi() {
+    local seq
+    declare -a mycodes=("${!1}")
+
+    debug "ansi: ${!1} all: $* aka ${mycodes[@]}"
+    
+    seq=""
+    for ((i = 0; i < ${#mycodes[@]}; i++)); do
+	if [[ -n $seq ]]; then
+	    seq="${seq};"
+	fi
+	seq="${seq}${mycodes[$i]}"
+    done
+    debug "ansi debug:" '\\[\\033['${seq}'m\\]'
+    echo -ne '\[\033['${seq}'m\]'
+#    PR="$PR\[\033[${seq}m\]"
+}
+
+ansi_single() {
+    echo -ne '\[\033['$1'm\]'
+}
+
+# Begin a segment
+# Takes two arguments, background and foreground. Both can be omitted,
+# rendering default background/foreground.
+prompt_segment() {
+    local bg fg
+    declare -a codes
+
+    debug "Prompting $1 $2 $3"
+
+    # if commented out from kruton's original... I'm not clear
+    # if it did anything, but it messed up things like
+    # prompt_status - Erik 1/14/17
+
+    #    if [[ -z $1 || ( -z $2 && $2 != default ) ]]; then
+    codes=("${codes[@]}" $(text_effect reset))
+    #    fi
+    if [[ -n $1 ]]; then
+	bg=$(bg_color $1)
+	codes=("${codes[@]}" $bg)
+	debug "Added $bg as background to codes"
+    fi
+    if [[ -n $2 ]]; then
+	fg=$(fg_color $2)
+	codes=("${codes[@]}" $fg)
+	debug "Added $fg as foreground to codes"
+    fi
+
+    debug "Codes: "
+#    declare -p codes
+
+    if [[ $CURRENT_BG != NONE && $1 != $CURRENT_BG ]]; then
+	declare -a intermediate=($(fg_color $CURRENT_BG) $(bg_color $1))
+	debug "pre prompt " $(ansi intermediate[@])
+	PR="$PR $(ansi intermediate[@])$SEGMENT_SEPARATOR"
+	debug "post prompt " $(ansi codes[@])
+	PR="$PR$(ansi codes[@]) "
+    else
+	debug "no current BG, codes is $codes[@]"
+	PR="$PR$(ansi codes[@]) "
+    fi
+    CURRENT_BG=$1
+    [[ -n $3 ]] && PR="$PR$3"
+}
+
+# End the prompt, closing any open segments
+prompt_end() {
+    if [[ -n $CURRENT_BG ]]; then
+	declare -a codes=($(text_effect reset) $(fg_color $CURRENT_BG))
+	PR="$PR $(ansi codes[@])$SEGMENT_SEPARATOR"
+    fi
+    declare -a reset=($(text_effect reset))
+    PR="$PR $(ansi reset[@])"
+    CURRENT_BG=''
+}
+
+
+######################################################################
+#
+# experimental right prompt stuff
+# requires setting prompt_foo to use PRIGHT vs PR
+# doesn't quite work per above
+
+rightprompt() {
+    printf "%*s" $COLUMNS "$PRIGHT"
+}
+
+# quick right prompt I grabbed to test things.
+__command_rprompt() {
+	local times= n=$COLUMNS tz
+	for tz in ZRH:Europe/Zurich PIT:US/Eastern \
+	          MTV:US/Pacific TOK:Asia/Tokyo; do
+		[ $n -gt 40 ] || break
+		times="$times ${tz%%:*}\e[30;1m:\e[0;36;1m"
+		times="$times$(TZ=${tz#*:} date +%H:%M)\e[0m"
+		n=$(( $n - 10 ))
+	done
+	[ -z "$times" ] || printf "%${n}s$times\\r" ''
+}
+# PROMPT_COMMAND=__command_rprompt
+
+# this doens't wrap code in \[ \]
+ansi_r() {
+    local seq
+    declare -a mycodes2=("${!1}")
+
+    debug "ansi: ${!1} all: $* aka ${mycodes2[@]}"
+    
+    seq=""
+    for ((i = 0; i < ${#mycodes2[@]}; i++)); do
+	if [[ -n $seq ]]; then
+	    seq="${seq};"
+	fi
+	seq="${seq}${mycodes2[$i]}"
+    done
+    debug "ansi debug:" '\\[\\033['${seq}'m\\]'
+    echo -ne '\033['${seq}'m'
+#    PR="$PR\[\033[${seq}m\]"
+}
+
+# Begin a segment on the right
+# Takes two arguments, background and foreground. Both can be omitted,
+# rendering default background/foreground.
+prompt_right_segment() {
+    local bg fg
+    declare -a codes
+
+    debug "Prompt right"
+    debug "Prompting $1 $2 $3"
+
+    # if commented out from kruton's original... I'm not clear
+    # if it did anything, but it messed up things like
+    # prompt_status - Erik 1/14/17
+
+    #    if [[ -z $1 || ( -z $2 && $2 != default ) ]]; then
+    codes=("${codes[@]}" $(text_effect reset))
+    #    fi
+    if [[ -n $1 ]]; then
+	bg=$(bg_color $1)
+	codes=("${codes[@]}" $bg)
+	debug "Added $bg as background to codes"
+    fi
+    if [[ -n $2 ]]; then
+	fg=$(fg_color $2)
+	codes=("${codes[@]}" $fg)
+	debug "Added $fg as foreground to codes"
+    fi
+
+    debug "Right Codes: "
+#    declare -p codes
+
+    # right always has a separator
+#    if [[ $CURRENT_RBG != NONE && $1 != $CURRENT_RBG ]]; then
+#	$CURRENT_RBG=
+#    fi
+    declare -a intermediate2=($(fg_color $1) $(bg_color $CURRENT_RBG) )
+#    PRIGHT="$PRIGHT---"
+    debug "pre prompt " $(ansi_r intermediate2[@])
+    PRIGHT="$PRIGHT$(ansi_r intermediate2[@])$RIGHT_SEPARATOR"
+    debug "post prompt " $(ansi_r codes[@])
+    PRIGHT="$PRIGHT$(ansi_r codes[@]) "
+ #   else
+#	debug "no current BG, codes is $codes[@]"
+#	PRIGHT="$PRIGHT$(ansi codes[@]) "
+#    fi
+    CURRENT_RBG=$1
+    [[ -n $3 ]] && PRIGHT="$PRIGHT$3"
+}
+
+
+### Prompt components
+# Each component will draw itself, and hide itself if no information needs to be shown
+
+# Context: user@hostname (who am I and where am I)
+prompt_time() {
+    prompt_segment magenta white "\t"
+}
+
+# Context: user@hostname (who am I and where am I)
+prompt_context() {
+    prompt_segment blue white "\h"
+}
+
+# Dir: current working directory
+prompt_dir() {
+    prompt_segment blue_light black '\W'
+}
+
+
+# Git: branch/detached head, dirty status
+git_status_dirty() {
+    dirty=$(git status -s 2> /dev/null | tail -n 1)
+    [[ -n $dirty ]] && echo " ●"
+}
+
+prompt_git() {
+    local ref dirty
+    if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
+	ZSH_THEME_GIT_PROMPT_DIRTY='±'
+	dirty=$(git_status_dirty)
+	ref=$(git symbolic-ref HEAD 2> /dev/null) || ref="➦ $(git show-ref --head -s --abbrev |head -n1 2> /dev/null)"
+	if [[ -n $dirty ]]; then
+	    prompt_segment yellow black
+	else
+	    prompt_segment green black
+	fi
+	PR="$PR${ref/refs\/heads\// }$dirty "
+    fi
+}
+
+# Status:
+# - was there an error
+# - am I root
+# - are there background jobs?
+prompt_status() {
+    local symbols
+    symbols=()
+    if [[ $RETVAL -ne 0 ]]; then
+        symbols+="$(ansi_single $(fg_color red))✘"
+    else
+        symbols+="$(ansi_single $(fg_color green))✓"
+    fi
+    [[ $UID -eq 0 ]] && symbols+="$(ansi_single $(fg_color yellow))⚡"
+    [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="$(ansi_single $(fg_color cyan))⚙"
+
+    [[ -n "$symbols" ]] && prompt_segment black default "$symbols"
+}
+
+######################################################################
+## Main prompt
+
+build_prompt() {
+    prompt_time
+    prompt_context
+    prompt_dir
+    prompt_git
+    prompt_status
+    prompt_end
+   
+}
+
+# from orig... 
+# export PS1='$(ansi_single $(text_effect reset)) $(build_prompt) '
+# this doesn't work... new model: create a prompt via a PR variable and
+# use that.
+
+set_bash_prompt() {
+    RETVAL=$?
+    PR=""
+    PRIGHT=""
+    CURRENT_BG=NONE
+    PR="$(ansi_single $(text_effect reset))"
+    build_prompt
+
+    # uncomment below to use right prompt
+    #     PS1='\[$(tput sc; printf "%*s" $COLUMNS "$PRIGHT"; tput rc)\]'$PR
+    PS1=$PR
+}
+
+PROMPT_COMMAND=set_bash_prompt
