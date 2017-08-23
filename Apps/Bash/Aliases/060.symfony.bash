414f2518005833e28c9b3b0946741e92bdcb9256
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2017-08-23 10:41:47 +0100

    2017-08-23 10:40

--- Apps/Bash/Aliases/060.symfony.bash
+++ Apps/Bash/Aliases/060.symfony.bash
@@ -43,3 +43,42 @@ function symAssets()
 		php -d memory_limit=-1 php/console assets:install --env=prod
     fi          
 }
+
+function symDoctrineRecreate()
+{
+    if [ ! -d app/config ]; then
+		return 0
+    fi
+    
+    if [ -f bin/console ]; then
+		php -d memory_limit=-1 bin/console doctrine:database:drop --force
+		php -d memory_limit=-1 bin/console doctrine:database:create
+		php -d memory_limit=-1 bin/console doctrine:schema:create
+		php -d memory_limit=-1 bin/console doctrine:fixtures:load
+    fi     
+    
+    if [ -f app/console ]; then
+		php -d memory_limit=-1 app/console doctrine:database:drop --force
+		php -d memory_limit=-1 app/console doctrine:database:create
+		php -d memory_limit=-1 app/console doctrine:schema:create
+		php -d memory_limit=-1 app/console doctrine:fixtures:load
+    fi              
+}
+
+function symDoctrineUpdate()
+{
+    if [ ! -d app/config ]; then
+		return 0
+    fi
+    
+    if [ -f bin/console ]; then
+		php -d memory_limit=-1 bin/console doctrine:schema:update -f
+		php -d memory_limit=-1 bin/console doctrine:schema:validate
+    fi     
+    
+    if [ -f app/console ]; then
+		php -d memory_limit=-1 app/console doctrine:schema:update -f
+		php -d memory_limit=-1 app/console doctrine:schema:validate
+    fi              
+}
+
