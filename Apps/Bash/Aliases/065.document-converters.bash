51543edaed80c8e2a9c121d6c31953f913668b1b
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2017-11-08 14:49:44 +0000

    2017-11-08 14:35

---
+++ Apps/Bash/Aliases/065.document-converters.bash
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+# vi:syntax=bash
+
+##### Converts Markdown to HTML
+function mdToHTML()
+{
+    if [ -z "$1" ]; then
+        printError "The source document was not provided."
+        return 255
+    fi
+    
+    if [ ! -f "$1" ]; then
+        printError "The source document does not exist."
+        return 255
+    fi
+
+    folder=$(dirname "${1}")
+    file=$(basename "${1}")
+    extension="${file##*.}"
+	fileName="${file%.*}"
+	newFileName="${folder}/${fileName}.html"
+
+	if [ -z "$2" ]; then
+		pandoc "${1}" --output="${newFileName}" --to=html5 \
+		--highlight-style=haddock --self-contained --smart
+	else
+		pandoc "${1}" --output="${newFileName}" --to=html5 \
+		--highlight-style=haddock --self-contained --smart \
+		--css="${2}"
+	fi
+	
+	echo "${newFileName}"
+}
+
+function htmlToMd()
+{
+    if [ -z "$1" ]; then
+        printError "The source document was not provided."
+        return 255
+    fi
+    
+    if [ ! -f "$1" ]; then
+        printError "The source document does not exist."
+        return 255
+    fi
+
+    folder=$(dirname "${1}")
+    file=$(basename "${1}")
+    extension="${file##*.}"
+	fileName="${file%.*}"
+	newFileName="${folder}/${fileName}.pdf"
+	
+	if [ -z "$2" ]; then
+		wkhtmltopdf --page-size A4 "${1}" "${newFileName}"
+	else
+		wkhtmltopdf --page-size A4 --user-style-sheet "${2}" \
+		"${1}" "${newFileName}"	
+	fi
+	
+	echo "${newFileName}"
+}
+
+mdToPdf()
+{
+    if [ -z "$1" ]; then
+        printError "The source document was not provided."
+        return 255
+    fi
+    
+    if [ ! -f "$1" ]; then
+        printError "The source document does not exist."
+        return 255
+    fi
+    
+    folder=$(dirname "${1}")
+    file=$(basename "${1}")
+    extension="${file##*.}"
+	fileName="${file%.*}"
+	htmlFileName="/tmp/${fileName}.html"
+	newFileName="${folder}/${fileName}.pdf"
+	
+	
+	if [ -z "$2" ]; then
+		pandoc "${1}" --output="${htmlFileName}" --to=html \
+		--highlight-style=haddock --self-contained --smart
+		
+		wkhtmltopdf --page-size A4 "${htmlFileName}" "${newFileName}"
+	else
+		pandoc "${1}" --output="${htmlFileName}" --to=html \
+		--highlight-style=haddock --self-contained --smart \
+		--css="${2}"
+		
+		wkhtmltopdf --page-size A4 --user-style-sheet "${2}" \
+		"${htmlFileName}" "${newFileName}"
+	fi	
+
+	echo "${newFileName}"
+}
